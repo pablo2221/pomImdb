3cf06be7d788f01f41ec100c3d42e9b1bba18938
pablo2221 <pablo.2221@hotmail.com>
2018-02-20 08:23:22 -0600

    Imdb code
    find a movie in Imdb and verifies director

--- src/tests/PomImdbTest.java
+++ src/tests/PomImdbTest.java
@@ -11,25 +11,18 @@ public class PomImdbTest extends ParentTest {
 	@Test
 	public void testFindMovie() {
 		
-		/*paginaPrincipal
-		buscarPelicula
-		WebElement campoBusqueda, botonBusqueda*/
+		/*paginaPrincipal > buscarPelicula > WebElement campoBusqueda, botonBusqueda*/
 		
 		PaginaPrincipal pagPrincipal = new PaginaPrincipal();
 		pagPrincipal.buscarPelicula("Thor");
 		
-		/*PaginasResultadosDePeliculas
-		verificarPeliculaCiertaFecha
-		abrirPeliculaCiertaFecha
-		WebElement seccionTitulosPelicula*/
+		/*PaginasResultadosDePeliculas > verificarPeliculaCiertaFecha > abrirPeliculaCiertaFecha > WebElement seccionTitulosPelicula*/
 		
 		PaginasResultadosPeliculas pagResultadosPelicula = new PaginasResultadosPeliculas();
 		pagResultadosPelicula.verificarPeliculaCiertaFecha("Thor", "2011");
 		pagResultadosPelicula.abrirPeliculaCiertaFecha("Thor","2011");
 
-		/*pa  ginaDetallesDePelicula
-		 verificarDirector
-		 WebElement nombreDirectorPelicula*/
+		/*paginaDetallesDePelicula > verificarDirector > WebElement nombreDirectorPelicula*/
 		
 		PaginaDetallesPelicula pagDetallesPelicula = new PaginaDetallesPelicula();
 		pagDetallesPelicula.verificarDirector("Kenneth Branagh");
